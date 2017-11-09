349f6b17e62acb42cc646e7f0047708a34590ab4
VPanasiuk <vpanasiuk@clarus.tech>
2017-11-09 19:08:34 +0300

    Start testing. Start filtering with JS

--- main/resources/static/js/getSortList.js
+++ main/resources/static/js/getSortList.js
@@ -34,12 +34,49 @@ $(document).ready(function () {
         }
 
         getList("name")
+
         $("#sortByName").click(function () {
                 getList("name")
             }
         )
+
         $("#sortByPrice").click(function () {
                 getList("price")
+            }
+        )
+
+        $("#filterMoreButton").click(function () {
+            $.ajax({
+                type: "GET",
+                url: "api/apartment/all?sort=" + $('#filterMoreField').val,
+                success: function (result) {
+                    if (result.status === "Done") {
+                        $('#tbodyId tr').empty();
+                        $.each(result.data, function (i, apart) {
+                            var customer = "<tr><td>" +
+                                apart.name +
+                                "</td><td>" +
+                                apart.description +
+                                "</td><td>" +
+                                apart.price +
+                                "</td><td><a href=\"" +
+                                "/buyApartment/" +
+                                apart.id +
+                                "\" class=\"btn btn-primary\">" +
+                                "Detail" +
+                                "</a></td></tr>";
+                            $('#tbodyId').append(customer)
+                        });
+                        console.log("Success: ", result);
+                    } else {
+                        console.log("Fail: ", result);
+                    }
+                },
+                error: function (e) {
+                    console.log("ERROR: ", e);
+                }
+            });
+
             }
         )
     }
